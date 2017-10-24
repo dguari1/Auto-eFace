86bbcf4b043cf16b1ebdc28b3e3a354ddc2f24c9
Diego L. Guarin <diego.guarinlopez@mail.mcgill.ca>
2017-10-24 11:53:09 -0400

    more

--- DragandDrop-image.py
+++ DragandDrop-image.py
@@ -14,6 +14,8 @@ from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5 import QtCore
 
+from PyQt5.QtCore import QFile, QTextStream
+
 """
 This class is in charge of drawing the picture and the landmarks in the main 
 window, it also takes care of lifting and re-location of landmarks. 
@@ -43,13 +45,22 @@ class ThumbNailViewer(QtWidgets.QGraphicsView):
         self.setAcceptDrops(True)
         self.dragOver = False
         
-
+        
         
         
         self._validextensions = ['.png', '.jpg', '.jpeg', '.tif', '.tiff', '.PNG', '.JPG', '.JPEG', '.TIF', '.TIFF']
+        
+        self.setBackground()
 
         #QtWidgets.QGraphicsView.RubberBandDrag
         
+        
+    def setBackground(self):
+        scriptDir = os.path.dirname(os.path.realpath(sys.argv[0]))
+        pixmap = QtGui.QPixmap(scriptDir + os.path.sep + 'include' + os.path.sep + 'drop_image.png')
+        
+        self.setPhoto(pixmap)
+               
     def setPhoto(self, pixmap = None):
         #this function puts an image in the scece (if pixmap is not None), it
         #sets the zoom to zero  
@@ -100,6 +111,7 @@ class ThumbNailViewer(QtWidgets.QGraphicsView):
                 if extension in self._validextensions:
                     event.setDropAction(QtCore.Qt.CopyAction)
                     event.accept()
+                    #self.setPhoto()
                 else:
                     event.ignore()
         else:
@@ -134,46 +146,98 @@ class window(QtWidgets.QWidget):
         self.initUI()
         
     def initUI(self):
-        
+        scriptDir = os.path.dirname(os.path.realpath(sys.argv[0]))
         
         spacerh = QtWidgets.QWidget(self)
-        spacerh.setFixedSize(10,0)
+        spacerh.setFixedSize(15,0)
         
         spacerv = QtWidgets.QWidget(self)
-        spacerv.setFixedSize(0,10)
+        spacerv.setFixedSize(0,15)
         
         #the image will be displayed in the custom ImageViewer
         self.Rest = ThumbNailViewer() 
         self.Rest.setMinimumWidth(100)
         self.Rest.setMinimumHeight(150)
-        self.Rest_title = QtWidgets.QLabel()
-        self.Rest_title.setText('Rest:')
-        self.Rest_title.setWordWrap(True)
+        RestBox = QtWidgets.QGroupBox('Rest')
+        RestBox.setStyleSheet(self.getStyleSheet(scriptDir + os.path.sep + 'include' + os.path.sep + 'GroupBoxStyle.qss'))
+        #RestBox.setStyleSheet("QGroupBox {font-size: 16px;font-weight: bold;border: 1px solid silver; border-radius: 4px; margin-top: 6px;} ")
+#        RestBox.setStyleSheet('QGroupBox:title {'
+#                 'subcontrol-origin: margin;'
+#                 'subcontrol-position: top center;'
+#                 'padding-left: 10px;'
+#                 'padding-right: 10px; }')
+        RestLayout = QtWidgets.QGridLayout()
+        RestLayout.addWidget(self.Rest,0,0,1,1)
+        RestBox.setLayout(RestLayout)
+  
         
         self.SmallSmile = ThumbNailViewer()
         self.SmallSmile.setMinimumWidth(100)
         self.SmallSmile.setMinimumHeight(150)
-        self.SmallSmile_title = QtWidgets.QLabel()
-        self.SmallSmile_title.setText('Small Smile:')      
+        SmallSmileBox = QtWidgets.QGroupBox('Small Smile')
+        SmallSmileBox.setStyleSheet(self.getStyleSheet(scriptDir + os.path.sep + 'include' + os.path.sep + 'GroupBoxStyle.qss'))
+        SmallSmileLayout = QtWidgets.QGridLayout()
+        SmallSmileLayout.addWidget(self.SmallSmile,0,0,1,1)
+        SmallSmileBox.setLayout(SmallSmileLayout)
         
         
         self.LargeSmile = ThumbNailViewer()
         self.LargeSmile.setMinimumWidth(100)
         self.LargeSmile.setMinimumHeight(150)
-        self.LargeSmile_title = QtWidgets.QLabel()
-        self.LargeSmile_title.setText('Large Smile:')   
+        LargeSmileBox = QtWidgets.QGroupBox('Large Smile')
+        LargeSmileBox.setStyleSheet(self.getStyleSheet(scriptDir + os.path.sep + 'include' + os.path.sep + 'GroupBoxStyle.qss'))
+        LargeSmileLayout = QtWidgets.QGridLayout()
+        LargeSmileLayout.addWidget(self.LargeSmile,0,0,1,1)
+        LargeSmileBox.setLayout(LargeSmileLayout)
+
         
         
         self.EyeBrow = ThumbNailViewer()
         self.EyeBrow.setMinimumWidth(100)
         self.EyeBrow.setMinimumHeight(150)
-        self.EyeBrow_title = QtWidgets.QLabel()
-        self.EyeBrow_title.setText('EyeBrow Elevation:')  
+        EyeBrowBox = QtWidgets.QGroupBox('EyeBrow Elevation')
+        EyeBrowBox.setStyleSheet(self.getStyleSheet(scriptDir + os.path.sep + 'include' + os.path.sep + 'GroupBoxStyle.qss'))
+        EyeBrowLayout = QtWidgets.QGridLayout()
+        EyeBrowLayout.addWidget(self.EyeBrow,0,0,1,1)
+        EyeBrowBox.setLayout(EyeBrowLayout)
         
-        Rest = QtWidgets.QGroupBox('Rest')
-        RestLayout = QtWidgets.QGridLayout()
-        RestLayout.addWidget(self.Rest,0,0,1,2)
-        Rest.setLayout(RestLayout)
+        self.EyeClosureGently = ThumbNailViewer()
+        self.EyeClosureGently.setMinimumWidth(100)
+        self.EyeClosureGently.setMinimumHeight(150)
+        EyeClosureGentlyBox = QtWidgets.QGroupBox('Gently Eye Closure')
+        EyeClosureGentlyBox.setMinimumWidth(100)
+        EyeClosureGentlyBox.setMinimumHeight(150)
+        EyeClosureGentlyBox.setStyleSheet(self.getStyleSheet(scriptDir + os.path.sep + 'include' + os.path.sep + 'GroupBoxStyle.qss'))
+        EyeClosureGentlyLayout = QtWidgets.QGridLayout()
+        EyeClosureGentlyLayout.addWidget(self.EyeClosureGently,0,0,1,1)
+        EyeClosureGentlyBox.setLayout(EyeClosureGentlyLayout)
+        
+        self.EyeClosureTight = ThumbNailViewer()
+        self.EyeClosureTight.setMinimumWidth(100)
+        self.EyeClosureTight.setMinimumHeight(150)
+        EyeClosureTightBox = QtWidgets.QGroupBox('Tight Eye Closure')
+        EyeClosureTightBox.setStyleSheet(self.getStyleSheet(scriptDir + os.path.sep + 'include' + os.path.sep + 'GroupBoxStyle.qss'))
+        EyeClosureTightLayout = QtWidgets.QGridLayout()
+        EyeClosureTightLayout.addWidget(self.EyeClosureTight,0,0,1,1)
+        EyeClosureTightBox.setLayout(EyeClosureTightLayout)
+        
+        self.PuckeringLips = ThumbNailViewer()
+        self.PuckeringLips.setMinimumWidth(100)
+        self.PuckeringLips.setMinimumHeight(150)
+        PuckeringLipsBox = QtWidgets.QGroupBox('Puckering Lips')
+        PuckeringLipsBox.setStyleSheet(self.getStyleSheet(scriptDir + os.path.sep + 'include' + os.path.sep + 'GroupBoxStyle.qss'))
+        PuckeringLipsLayout = QtWidgets.QGridLayout()
+        PuckeringLipsLayout.addWidget(self.PuckeringLips,0,0,1,1)
+        PuckeringLipsBox.setLayout(PuckeringLipsLayout)
+        
+        self.DentalShow = ThumbNailViewer()
+        self.DentalShow.setMinimumWidth(100)
+        self.DentalShow.setMinimumHeight(150)
+        DentalShowBox = QtWidgets.QGroupBox('Showing theet')
+        DentalShowBox.setStyleSheet(self.getStyleSheet(scriptDir + os.path.sep + 'include' + os.path.sep + 'GroupBoxStyle.qss'))
+        DentalShowLayout = QtWidgets.QGridLayout()
+        DentalShowLayout.addWidget(self.DentalShow,0,0,1,1)
+        DentalShowBox.setLayout(DentalShowLayout)
         
         
 
@@ -184,29 +248,43 @@ class window(QtWidgets.QWidget):
         #layout.addWidget(self.Rest_title,0,0,1,1)
         #layout.addWidget(self.Rest,1,0,1,2)
         
-        layout.addWidget(Rest,0,0,2,2)
+        layout.addWidget(RestBox,0,0,1,1)
         
-        layout.addWidget(spacerv,0,2,2,1)
+        layout.addWidget(spacerv,0,1,2,1)
         
-        layout.addWidget(self.SmallSmile_title,0,3,1,1)
-        layout.addWidget(self.SmallSmile,1,3,1,2)        
+        layout.addWidget(SmallSmileBox,0,2,1,1)        
+        
+        layout.addWidget(spacerv,0,3,2,1)
+        
+        layout.addWidget(LargeSmileBox,0,4,1,1)       
         
         layout.addWidget(spacerv,0,5,2,1)
         
-        layout.addWidget(self.LargeSmile_title,0,6,1,1)
-        layout.addWidget(self.LargeSmile,1,6,1,2)       
+        layout.addWidget(EyeBrowBox ,0,6,1,1)  
         
-        layout.addWidget(spacerv,0,8,2,1)
+        layout.addWidget(spacerh,1,0,1,6)
         
-        layout.addWidget(self.EyeBrow_title,0,9,1,1)
-        layout.addWidget(self.EyeBrow ,1,9,1,2)     
+        layout.addWidget(EyeClosureGentlyBox,2,0,1,1)
         
+        layout.addWidget(EyeClosureTightBox,2,2,1,1)
+        
+        layout.addWidget(PuckeringLipsBox,2,4,1,1)
+        
+        layout.addWidget(DentalShowBox,2,6,1,1)
         
         self.setLayout(layout)
         
 
         
-        self.show()        
+        self.show()     
+        
+        
+    def getStyleSheet(self, path):
+        f = QFile(path)
+        f.open(QFile.ReadOnly | QFile.Text)
+        stylesheet = QTextStream(f).readAll()
+        f.close()
+        return stylesheet
             
 if __name__ == '__main__':
     
